a399edf390c186df648c394441598fbedaca6838
Ilion78 <37938374+Ilion78@users.noreply.github.com>
2020-12-01 17:06:30 +0100

    intro to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserComponent } from './user/user.component';
+import { Route, RouterModule } from '@angular/router';
+
+const appRoute: Route[] = [
+  {path: 'home', component: HomepageComponent},
+  {path: 'user', component: UserComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
